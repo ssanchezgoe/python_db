3c145347a7a7423351dfac546770636ce1919823
ssanchezgoe <sebassan182@gmail.com>
2020-08-19 21:07:10 -0500

    Hola Mundo con extensión  de líneas de comando

---
+++ hello_cl_ext.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    return '<h1>Hola Mundo!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hola, %s!</h1>' % name
+
+if __name__ == '__main__':
+    manager.run()
